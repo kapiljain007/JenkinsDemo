57598fd83636c41ade843ec13617bc219c7fec48
Kapil Jain <kapil.j@innovify.in>
2019-09-13 19:19:03 +0530

    Did changes in config

--- ConfigNew.js
+++ ConfigNew.js
@@ -74,8 +74,8 @@ exports.config = {
 			savePath: './Jasmine Reports',
 			takeScreenshots: true,
 			takeScreenshotsOnlyOnFailures: true,
-			// consolidate: false,
-			//    consolidateAll: false,
+			consolidate: false,
+			   consolidateAll: false,
 			   showPassed: false
 		}));
 
@@ -94,15 +94,15 @@ exports.config = {
 		 }).getJasmine2Reporter());
 		 
 	
-    jasmine.getEnv().addReporter(new AllureReporter());
-    jasmine.getEnv().afterEach(function(done){
-      browser.takeScreenshot().then(function (png) {
-        allure.createAttachment('Screenshot', function () {
-          return new Buffer(png, 'base64')
-        }, 'image/png')();
-        done();
-      })
-    });
+    // jasmine.getEnv().addReporter(new AllureReporter());
+    // jasmine.getEnv().afterEach(function(done){
+    //   browser.takeScreenshot().then(function (png) {
+    //     allure.createAttachment('Screenshot', function () {
+    //       return new Buffer(png, 'base64')
+    //     }, 'image/png')();
+    //     done();
+    //   })
+    // });
 
 		  var jasmineReporters = require('jasmine-reporters');
 		   jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
@@ -156,35 +156,35 @@ exports.config = {
 			
 			///Nodemailer Code
 
-			var nodemailer = require("nodemailer");
-			var transport = nodemailer.createTransport({
-			service: 'Gmail',
-    				auth: {
-        				user: "vujkdkolks@gmail.com",
-       					pass: "Test@123"
-    }
-					});
-		var mailOptions = {
-    		from: 'vujkdkolks@gmail.com', // sender address
-    		to: 'thisisqa2108@gmail.com', // list of receivers
-    		subject: 'Automation Result', // Subject line
-			//text: info.body,
-    		text: 'Contains the test result for the smoke test in html file', // plaintext body
-    attachments: [
-        {
-            'path': 'file:///C:/Testing/Automation_Generic_Framework/Automation_Generic_Framework/Pie-Chart%20Report/chrome-test-report.html',
-        }]
-		};
-transport.sendMail(mailOptions, function (error, info) {
-    if (error) {
-        console.log(error);
-		response.send(err);
-    } else {
-        console.log("Message sent: " + info.response);
-		response.send(info);
-    }
-});
-
+	// 		var nodemailer = require("nodemailer");
+	// 		var transport = nodemailer.createTransport({
+	// 		service: 'Gmail',
+    // 				auth: {
+    //     				user: "vujkdkolks@gmail.com",
+    //    					pass: "Test@123"
+    // }
+	// 				});
+	// 	var mailOptions = {
+    // 		from: 'vujkdkolks@gmail.com', // sender address
+    // 		to: 'thisisqa2108@gmail.com', // list of receivers
+    // 		subject: 'Automation Result', // Subject line
+	// 		//text: info.body,
+    // 		text: 'Contains the test result for the smoke test in html file', // plaintext body
+    // attachments: [
+    //     {
+    //         'path': 'file:///C:/Testing/Automation_Generic_Framework/Automation_Generic_Framework/Pie-Chart%20Report/chrome-test-report.html',
+    //     }]
+	// 	};
+// transport.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//         console.log(error);
+// 		response.send(err);
+//     } else {
+//         console.log("Message sent: " + info.response);
+// 		response.send(info);
+//     }
+// });
+			
 	  },
 
 	  
